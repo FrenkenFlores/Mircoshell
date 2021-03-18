dd5fbb6d47eaa48d818c9ba21439c962301279df
FrenkenFlores <saifualdin.evloev@gmail.com>
2021-03-18 16:51:35 +0300

    update #5: added some color

--- microshell/microshell_v2.c
+++ microshell/microshell_v2.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell_v2.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fflores <fflores@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/18 16:40:47 by fflores           #+#    #+#             */
+/*   Updated: 2021/03/18 16:50:19 by fflores          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
@@ -15,6 +27,8 @@ void    type_prompt()
 		char *ANSI_CLEAR_SCREEN_ESCAPE_CODE = "\e[2J";
 		write(1, ANSI_MOVE_CURSOR_ESCAPE_CODE, 6);
 		write(1, ANSI_CLEAR_SCREEN_ESCAPE_CODE, 4);
+		write(1, "\e[3m\e[32mFFLORES\n", strlen("\e[3m\e[32mFFLORES\n"));
+		write(1, "\e[0m", strlen("\e[0m"));
 		first_time = 0;
 	}
 	write(1, "$", 1);
@@ -75,13 +89,11 @@ char *concatenate_bin(char *command)
 {
 	int i;
 	int j;
-	int k;
 	char *s = "/bin/";
 	char *ptr = (char *)malloc(sizeof(char) * (strlen(command) + strlen(s) + 1));
 
 	i = -1;
 	j = -1;
-	k = -1;
 	while (s[++i] != '\0')
 		ptr[i] = s[i];
 	while (command[++j] != '\0')
@@ -103,9 +115,9 @@ int main(int argc, char **argv, char **envp)
 	{
 		type_prompt();
 		take_input(command, parameters);
-		bash_command = concatenate_bin(command);
 		if (strcmp(command, "exit") == 0)
 			exit(0);
+		bash_command = concatenate_bin(command);
 		// print_input(bash_command, parameters);
 		if ((fork_ret = fork()) != 0)
 			waitpid(-1, status, 0);
@@ -113,8 +125,9 @@ int main(int argc, char **argv, char **envp)
 			exec_ret = execve(bash_command, parameters, envp);
 		if (exec_ret == -1)
 		{
-			write(1, "Command not found\n", strlen("command not found\n"));
+			write(1, "\e[31mCommand not found\n", strlen("\e[31mcommand not found\n"));
 			exec_ret = 0;
+			write(1, "\e[0m", strlen("\e[0m"));
 			break ;
 		}
 	}
