8e4ab4c0d8e8ccd9629dfe5c916f42648147480b
FrenkenFlores <saifualdin.evloev@gmail.com>
2021-03-18 12:31:38 +0300

    update #1: mircoshell_v1

---
+++ microshell/microshell_v1.c
@@ -0,0 +1,84 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fflores <fflores@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/17 17:20:44 by fflores           #+#    #+#             */
+/*   Updated: 2021/03/18 12:28:44 by fflores          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "microshell.h"
+
+
+void type_prompt()
+{
+	static int first_time = 1;
+	if (first_time)
+	{
+		char *CLEAR_SCREEN_ANSI = " \e[1;1H\e[2J";
+		write(STDOUT_FILENO, CLEAR_SCREEN_ANSI, 12);
+		first_time = 0;
+	}
+	write(1, "$", 1);
+}
+
+void read_command ( char *command, char **parameters)
+{
+	char line[1024];
+	int count = 0;
+	int i = 0;
+	int j = 0;
+	char *array[100];
+	char *buf;
+	char *pch;
+	
+	while (1)
+	{
+		int c = fgetc (stdin);
+		line[count++] = (char)c;
+		if (c == '\n')
+			break;
+	}
+	if (count == 1)
+		return ;
+	pch = strtok(line, " \n");	// ' ' or '\n'
+	while (pch != NULL)			// get words (command and parameters) from the input line
+	{
+		array[i++] = strdup(pch);
+		pch = strtok(NULL, " \n");
+	}
+	strcpy(command, array[0]); //command
+	while (j < i)
+	{
+		parameters[j] = array[j];		// copying parameters
+		j++;
+	}
+	parameters[j] = NULL; // terminating parameters array with NULL
+}
+
+
+int main(int argc, char **argv, char **envp)
+{
+	char cmd[100];
+	char command[100];
+	char *parameters[20];
+	while (1)
+	{
+		type_prompt();
+		read_command( command, parameters );
+		if (fork() != 0)
+			wait(NULL);
+		else
+		{
+			strcpy( cmd, "/bin/" );
+			strcat( cmd, command );
+			execve( cmd, parameters, envp );
+		}
+		if ( strcmp( command, "exit" ) == 0)
+			break;
+	}
+	return (0);
+}
---
+++ microshell/microshell_v1.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   microshell.h                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fflores <fflores@student.21-school.ru>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/03/17 17:20:40 by fflores           #+#    #+#             */
+/*   Updated: 2021/03/18 12:28:10 by fflores          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MICROSHELL_H
+#define MICROSHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <signal.h>
+
+
+#endif
